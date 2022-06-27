a9d1c4c8d7e15f590b36605331eaca42a8157060
Owen Dearman <owendearman@gmail.com>
2022-06-27 10:59:30 +0100

    header created, CSS stylesheet implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Header } from "./Header";
+
 function App(): JSX.Element {
-  return <h1>Hello World</h1>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <img
+        className="header-image"
+        src="headerImage.png"
+        alt="Max Verstappen, Red Bull Racing"
+      />
+    </header>
+  );
+}
